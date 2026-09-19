502776cc65b16465f768d0d78cbe3b50d77f4c53
TAMIL <belivertamil57@gmail.com>
2026-09-19 19:32:35 +0530

    Improve movie booking UI

--- src/main/java/movie_ticket_booking/controller/HomeController.java
+++ src/main/java/movie_ticket_booking/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.moviebooking.controller;
+package movie_ticket_booking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
